06dfd97370ab2368e52a39d0f4540a7a3cc2e7fe
Zabrina L <zabrinatan@Zabrinas-MacBook-Pro.local>
2018-06-25 19:47:59 +1000

    added condition: only 1 image in each box

--- js/main.js
+++ js/main.js
@@ -2,91 +2,124 @@
 let player;
 let ai;
 
-if(player ==='circle') {
-  ai ='cross'
+if (player === 'circle') {
+  ai = 'cross'
 }
-$(document).ready(function(){
-//put image inside squares when square is clicked.
-  $('.one').on('click' , function() {
-    $('.one').append(`<img src ="images/${player}.png">`)
+$(document).ready(function() {
+      //put image inside squares when square is clicked.
+
+      $('.one').on('click', function() {
+          if ($('.one img').length === 0) {
+        $('.one').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+
+      $('.two').on('click', function() {
+          if ($('.two img').length === 0) {
+        $('.two').append(`<img src ="images/${player}.png">`)
+      }else {
+        return;
+      }
+    })
+
+      $('.three').on('click', function() {
+          if ($('.three img').length === 0) {
+        $('.three').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+      $('.four').on('click', function() {
+          if ($('.four img').length === 0) {
+        $('.four').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+      $('.five').on('click', function() {
+        if ($('.five img').length === 0) {
+        $('.five').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+      $('.six').on('click', function() {
+        if ($('.six img').length === 0) {
+        $('.six').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+      $('.seven').on('click', function() {
+        if ($('.seven img').length === 0) {
+        $('.seven').append(`<img src ="images/${player}.png">`)
+      }else {
+        return;
+      }
+    })
+      $('.eight').on('click', function() {
+        if ($('.eight img').length === 0) {
+        $('.eight').append(`<img src ="images/${player}.png">`)
+      } else {
+        return;
+      }
+    })
+      $('.nine').on('click', function() {
+        if ($('.nine img').length === 0) {
+        $('.nine').append(`<img src ="images/${player}.png">`)
+      }else {
+       return;
+    }
   })
 
 
-  $('.two').on('click' , function() {
-    $('.two').append(`<img src ="images/${player}.png">`)
-  })
+              //change icon images
 
-  $('.three').on('click' , function() {
-    $('.three').append(`<img src ="images/${player}.png">`)
-  })
-  $('.four').on('click' , function() {
-    $('.four').append(`<img src ="images/${player}.png">`)
-  })
-  $('.five').on('click' , function() {
-    $('.five').append(`<img src ="images/${player}.png">`)
-  })
-  $('.six').on('click' , function() {
-    $('.six').append(`<img src ="images/${player}.png">`)
-  })
-  $('.seven').on('click' , function() {
-    $('.seven').append(`<img src ="images/${player}.png">`)
-  })
-  $('.eight').on('click' , function() {
-    $('.eight').append(`<img src ="images/${player}.png">`)
-  })
-  $('.nine').on('click' , function() {
-    $('.nine').append(`<img src ="images/${player}.png">`)
-  })
+              $('.circle').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('circ');
+                player = 'circle';
+                ai = 'cross';
+              })
 
-//make if statement to only allow 1 image in each div at one time.
+              $('.cross').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('x');
+                player = 'cross';
+                ai = 'circle';
+              })
 
+              $('.apple').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('app');
+                player = 'apple';
+                ai = 'window';
+              })
 
+              $('.windows').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('window');
+                player = 'window';
+                ai = 'apple';
+              })
 
-  //change icon images
+              $('.knight').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('horse');
+                player = 'knight';
+                ai = "rook";
+              })
 
-  $('.circle').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('circ');
-    player = 'circle';
-    ai = 'cross';
-  })
-
-  $('.cross').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('x');
-    player = 'cross';
-    ai = 'circle';
-  })
-
-  $('.apple').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('app');
-    player = 'apple';
-    ai = 'window';
-  })
-
-  $('.windows').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('window');
-    player = 'window';
-    ai = 'apple';
-  })
-
-  $('.knight').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('horse');
-    player = 'knight';
-    ai = "rook";
-  })
-
-  $('.rook').on('click', function() {
-    $('body').removeClass();
-    $('body').addClass('piece');
-    player = 'rook';
-    ai = "knight";
-  })
+              $('.rook').on('click', function() {
+                $('body').removeClass();
+                $('body').addClass('piece');
+                player = 'rook';
+                ai = "knight";
+              })
 
 
 
 
-})
+            })
